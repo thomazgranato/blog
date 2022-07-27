69c59b70dde9857d3b4999f178d4c31761d70549
thomazgranato <thomazgranato@gmail.com>
2022-07-27 00:34:34 +0000

    criado o base.html, criado o css style

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("Hello!")
 
-@app.route("/") 
-@app.route("/hello")
-def hello(): 
-    return "Hello World"
+posts = [
+    {
+        "title": "o meu primeiro Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Granato",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "o meu segundo Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Feulo",
+        "created": datetime(2022,7,26)
+    },
+]
 
-@app.route("/meucontato")
-def meuContao():
-    return render_template("index.html")
+@app.route("/") 
+def index(): 
+    return render_template("index.html", posts=posts)
+
+@app.route( '/login')  
+def login():
+    return render_template("login.html")
