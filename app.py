1e31d6f528b8d1d934d38088314bee3b66db6dfd
thomazgranato <thomazgranato@gmail.com>
2022-07-28 17:32:45 +0000

    Criado o Banco e sistema de user

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask("Hello!")
+app = Flask("hello")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-posts = [
-    {
-        "title": "o meu primeiro Post",
-        "body": "Aqui é o texto do Post",
-        "author": "Granato",
-        "created": datetime(2022,7,25)
-    },
-    {
-        "title": "o meu segundo Post",
-        "body": "Aqui é o texto do Post",
-        "author": "Feulo",
-        "created": datetime(2022,7,26)
-    },
-]
+db = SQLAlchemy(app)
 
-@app.route("/") 
-def index(): 
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), unique=True, index=True, nullable=False)
+    email = db.Column(db.String(70), unique=True,  nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author') 
+
+db.create_all()
+
+
+@app.route("/")
+def index():
+    posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
-@app.route( '/login')  
-def login():
-    return render_template("login.html")
+
+@app.route("/populate")
+def populate():
+    user = User(username='feulo', email="e@g.com", password_hash='a')
+    post1 = Post(title="Post 1", body="Texto do Post 1", author=user)
+    post2 = Post(title="Post 2", body="Texto do Post 2", author=user)
+    db.session.add(user)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    return redirect(url_for('index'))
