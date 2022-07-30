3f5bbf7ccbaa078fbb734c2c5b9a9db8786c9fb7
thomazgranato <thomazgranato@gmail.com>
2022-07-30 00:57:14 +0000

    corrigida a ordenação

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
-from flask_login import LoginManager, UserMixin, current_user, logout_user,  login_user, login_required
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
+
 import os
 
-app = Flask("hello")
-db_url = s.environ.get("DATABASE_URL") or "sqlite:///app.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace("postgres", "postgresql")
-# postgresql-metric-40498
 
+app = Flask("hello")
+db_url = os.environ.get("DATABASE_URL") or "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] =  db_url.replace("postgres", "postgresql") 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "pudim"
 
 db = SQLAlchemy(app)
 login = LoginManager(app)
-
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -26,13 +25,11 @@ class Post(db.Model):
     created = db.Column(db.DateTime, nullable=False, default=datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(20), unique=True,
-                         index=True, nullable=False)
-    email = db.Column(db.String(70), unique=True,  nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+    email = db.Column(db.String(64), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
     posts = db.relationship('Post', backref='author')
 
@@ -42,7 +39,6 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -50,14 +46,12 @@ def load_user(id):
 
 db.create_all()
 
-
 @app.route("/")
 def index():
-    posts = Post.query.all()
+    posts = Post.query.order_by(-Post.id).all()
     return render_template("index.html", posts=posts)
 
-
-@app.route("/register", methods=["GET", "POST"])
+@app.route('/register', methods=["GET","POST"])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -74,8 +68,7 @@ def register():
             flash("Username or E-mail already exists!")
         else:
             return redirect(url_for('login'))
-    return render_template("register.html")
-
+    return render_template('register.html')
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -90,16 +83,14 @@ def login():
             return redirect(url_for('login'))
         login_user(user)
         return redirect(url_for('index'))
-    return render_template('login.html')
-
+    return render_template("login.html")
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
-@app.route('/create', methods=['GET', 'POST'])
+@app.route('/create', methods=["GET", "POST"])
 @login_required
 def create():
     if request.method == "POST":
@@ -111,5 +102,5 @@ def create():
             db.session.commit()
             return redirect(url_for('index'))
         except IntegrityError:
-            flash("Error on create Post, try again later!")
+            flash("Error on create Post, try again later")
     return render_template('create.html')
